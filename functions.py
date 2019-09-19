cd06f43b6d1e708e2826848fe8f26e4beae80d1e
asif6666 <asifsaif6666@gmail.com>
2019-09-19 10:19:42 +0530

    sadad

---
+++ functions.py
@@ -0,0 +1,6 @@
+def add(x,y):
+    z=x+y
+    print(z)
+add(12,2)
+add(10,1)
+add(100,5)
---
+++ lar3.py
@@ -0,0 +1,9 @@
+a=input("enter the first number")
+b=input("enter the second number")
+c=input("enter the third number")
+if(a>b)and(b>c):
+    print(a)
+elif(b>a)and(b>c):
+    print(b)
+else:
+    print(c)  
